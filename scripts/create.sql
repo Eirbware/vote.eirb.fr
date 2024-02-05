f9ff0a079f6bc2027b7f449efd87761fa3a27f21
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2024-02-05 11:34:34 +0100

    Creation script done

--- scripts/create.sql
+++ scripts/create.sql
@@ -2,12 +2,9 @@
 -- EnregistrementVotes
 CREATE TABLE EnregistrementVotes(
     login VARCHAR(32) NOT NULL,
-    idCampagne INT NOT NULL
+    idCampagne INT NOT NULL,
+    PRIMARY KEY(login, idCampagne)
 );
-
-ALTER TABLE EnregistrementVotes
-ADD PRIMARY KEY(login, idCampagne);
-
 
 -- Campagnes
 CREATE TABLE Campagnes(
@@ -20,34 +17,31 @@ CREATE TABLE Campagnes(
     ecole VARCHAR(32) NOT NULL,
     PRIMARY KEY(id),
     CONSTRAINT debutAvantFin CHECK(dateDebut <= dateFin),
-    CONSTRAINT ouvertureAvantFermeture CHECK(dateOuvertureVotes <= dateFermetureVotes),
+    CONSTRAINT ouvertureAvantFermeture CHECK(dateOuvertureVotes <= dateFermetureVotes)
 );
-
 
 -- TypeCampagnes
 CREATE TABLE TypeCampagnes(
-    nomType VARCHAR(32) NOT NULL
+    nomType VARCHAR(32) NOT NULL,
+    PRIMARY KEY(nomType)
 );
-
-ALTER TABLE TypeCampagnes
-ADD PRIMARY KEY(nomType);
-
 
 -- Listes
 CREATE TABLE Listes(
     id INT NOT NULL AUTO_INCREMENT,
     idCampagne INT NOT NULL,
     nom VARCHAR(64) NOT NULL,
-    nbVotes INT NOT NULL
+    nbVotes INT NOT NULL,
+    PRIMARY KEY(id)
 );
 
-ALTER TABLE Listes
-ADD PRIMARY KEY(id);
 
-
-
--- ===== Contraintes de clés étrangères =====
+-- ===== Contraintes de clé étrangères =====
 ALTER TABLE EnregistrementVotes
+ADD CONSTRAINT
+FOREIGN KEY(idCampagne) REFERENCES Campagnes(id);
+
+ALTER TABLE Listes
 ADD CONSTRAINT
 FOREIGN KEY(idCampagne) REFERENCES Campagnes(id);
 
@@ -55,14 +49,11 @@ ALTER TABLE Campagnes
 ADD CONSTRAINT
 FOREIGN KEY(typeCampagne) REFERENCES TypeCampagnes(nomType);
 
-ALTER TABLE Listes
-ADD CONSTRAINT
-FOREIGN KEY(idCampagne) REFERENCES Campagnes(id);
 
 
--- ===== Enumérations =====
+-- ===== Énumérations =====
 INSERT INTO TypeCampagnes(nomType)
 VALUES ('BDE'),
        ('BDS'),
        ('BDA'),
-       ('Autre');
+       ('Autres');
