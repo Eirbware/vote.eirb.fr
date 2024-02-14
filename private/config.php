ae89e0b44edc75b92772ce9c349c8617c5adb81f
tbrintet <122631968+tbrintet@users.noreply.github.com>
2024-02-14 09:23:59 +0100

    config.php

--- private/config.php
+++ private/config.php
@@ -2,9 +2,9 @@
 
 $IDCAMPAGNE_EN_COURS = 1; // ID de la campagne en cours
 
-$host = 'localhost'; 
-$username = 'bot'; 
-$password = 'V0teB0t45!+'; 
-$database = 'voteirb'; 
+$host = 'localhost';
+$username = 'USERNAME';
+$password = 'PASSWORD';
+$database = 'DATABASE_NAME';
 
 ?>
