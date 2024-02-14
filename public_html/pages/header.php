52034e482fc6729f21d05b0a033f095b63037ed5
Thomnax <115790611+Thomnax@users.noreply.github.com>
2024-02-14 12:25:49 +0100

    remove code duplication and untrack config.php

--- public_html/pages/header.php
+++ public_html/pages/header.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Plateforme de Vote en ligne</title>
   <link rel="stylesheet" href="/assets/css/style.css">
   <style>
     .header {
