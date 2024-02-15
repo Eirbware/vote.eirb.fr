aefdfa4560321c751cdcecfdbadbe78e295d8344
tbrintet <122631968+tbrintet@users.noreply.github.com>
2024-02-15 15:24:12 +0100

    removed unused logout page

--- public_html/pages/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SESSION = array();
-// clear cas token GET variable
-$_GET = array();
-session_destroy();
-header("Location: https://cas.bordeaux-inp.fr/logout");
-exit();
-?>
