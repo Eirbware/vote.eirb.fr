4613dd3ba68c707cb85c75e18c6d582d895f62d6
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2024-02-16 15:29:46 +0100

    nicer admin page

---
+++ public_html/admin/index.php
@@ -0,0 +1,76 @@
+<?php
+require_once('../../private/config.php');
+
+require_once('../../private/auth.php');
+
+// define $_admins in private/config.php
+
+if (!in_array($_SESSION['user']['login'], $_admins)) {
+	redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
+
+// ====== Connexion à la base de données
+$conn = new mysqli($host, $username, $password, $database);
+if ($conn->connect_error) {
+    die("La connexion à la base de données a échoué: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Campagnes";
+$result = $conn->query($sql);
+
+function get_number_of_voting_users_for_campagne(int $id, mysqli $conn): int{
+    $sql = "SELECT COUNT(*) AS 'nbVoting' FROM EnregistrementVotes WHERE idCampagne = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+    if(!$stmt->execute()){
+        http_response_code(500);
+        exit;
+    }
+
+    $res = $stmt->get_result();
+    $ret = $res->fetch_assoc()["nbVoting"];
+    $stmt->close();
+
+    return $ret;
+}
+
+function print_votes_for_campagne_and_get_sum(int $id, mysqli $conn): int{
+    $sql = "SELECT * FROM Listes WHERE idCampagne = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+    if(!$stmt->execute()){
+        http_response_code(500);
+        exit;
+    }
+    $res = $stmt->get_result();
+    $sumVotes = 0;
+    while($row = $res->fetch_assoc()){
+        echo "<li>Liste '" . $row["nom"] . "':</li>";
+        echo "<ul>";
+        echo "<li>nbVotes: " . $row["nbVotes"] . "</li>";
+        echo "</ul>";
+        $sumVotes += $row["nbVotes"];
+    }
+    $stmt->close();
+    return $sumVotes;
+}
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "Campagne " . $row["id"] . ":";
+        echo "<ul>";
+        $sumVotes = print_votes_for_campagne_and_get_sum($row["id"], $conn);
+        if($sumVotes !== get_number_of_voting_users_for_campagne($row["id"], $conn)){
+            echo "<li><b>Votes incohérents!!!</b>";
+        }
+        echo "</ul>";
+    }
+} else {
+    echo "0 results";
+}
+$result->close();
+
+
+?>
+
+
