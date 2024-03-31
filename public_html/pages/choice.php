4e896c58ccae80b7d073891eaed2011b628a8c23
tbrintet <122631968+tbrintet@users.noreply.github.com>
2024-03-31 19:21:23 +0200

    Update choice.php for bds campaign

--- public_html/pages/choice.php
+++ public_html/pages/choice.php
@@ -34,18 +34,26 @@
 		</div>
 		<h3>Choix pour l'élection</h3>
 		<div class="cards">
-			<a href="/pages/confirmation.php?choix=mafieirb" rel="nofollow">
-				<img src="/assets/images/mafieirb.png" class="card-logo">
+			<a href="/pages/confirmation.php?choix=atlanteirb" rel="nofollow">
+				<img src="/assets/images/atlanteirb.png" class="card-logo">
 				<div class="card-text-box">
-					<h4>Mafi'eirb</h4>
+					<h4>Atlant'eirb</h4>
 					<p>Votez pour votre liste préférée</p>
 					<img src="/assets/images/arrow-right-short.svg" class="arrow" alt="flèche vers la droite">
 				</div>
 			</a>
-			<a href="/pages/confirmation.php?choix=dionyseirb" rel="nofollow">
-				<img src="/assets/images/dionyseirb.png" class="card-logo">
+			<a href="/pages/confirmation.php?choix=olympeirb" rel="nofollow">
+				<img src="/assets/images/olympeirb.png" class="card-logo">
 				<div class="card-text-box">
-					<h4>Dionys'eirb</h4>
+					<h4>Olymp'eirb</h4>
+					<p>Votez pour votre liste préférée</p>
+					<img src="/assets/images/arrow-right-short.svg" class="arrow" alt="flèche vers la droite">
+				</div>
+			</a>
+			<a href="/pages/confirmation.php?choix=lospollosheirbmanos" rel="nofollow">
+				<img src="/assets/images/lospollosheirbmanos.png" class="card-logo">
+				<div class="card-text-box">
+					<h4>Los Pollos Heirbmanos</h4>
 					<p>Votez pour votre liste préférée</p>
 					<img src="/assets/images/arrow-right-short.svg" class="arrow" alt="flèche vers la droite">
 				</div>
