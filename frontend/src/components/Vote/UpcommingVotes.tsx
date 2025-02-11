034b19ea4f118eca8ddb62e283baf7fdb765994b
Benjamin Grolleau <127044450+Grolleau-Benjamin@users.noreply.github.com>
2025-02-11 21:15:37 +0100

    feat(frontend): enhance UpcomingVotes component layout and add separator for vote lists

--- frontend/src/components/Vote/UpcommingVotes.tsx
+++ frontend/src/components/Vote/UpcommingVotes.tsx
@@ -51,7 +51,7 @@ export const UpcommingVotes = ({ upcommingVotes }: UpcommingVotesProps) => {
               return (
                 <div
                   key={campagne._id}
-                  className="flex flex-col border border-gray-300 rounded-lg p-4 gap-2"
+                  className="flex flex-col border border-gray-300 rounded-lg p-4 gap-2 md:max-w-4xl mx-auto"
                 >
                   <div className="flex flex-row justify-between w-full">
                     <p className="text-lg flex flex-row gap-2 items-center">
@@ -87,30 +87,39 @@ export const UpcommingVotes = ({ upcommingVotes }: UpcommingVotesProps) => {
                   <hr className="my-2 border-gray-300" />
 
                   <div className="flex flex-row items-center justify-around md:max-w-2xl mx-auto w-full">
-                    {(campagne.lists as IList[]).map((list: IList) => (
-                      <div className="flex flex-col gap-1" key={list._id}>
-                        <a
-                          href={list.site}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="flex flex-col items-center"
-                        >
-                          <img
-                            src={`/logos/lists/${list.filename}`}
-                            alt={list.name}
-                            className="w-16 h-16 object-cover rounded-full transition-transform duration-200 hover:scale-105"
-                          />
-                          <span className="text-center flex flex-row gap-1 justify-center hover:underline mt-1 ml-3">
-                            {list.trigram}
-                            <img
-                              src={LinkIcon}
-                              className="w-3 h-3"
-                              alt="Lien"
-                            />
-                          </span>
-                        </a>
-                      </div>
-                    ))}
+                    {(campagne.lists as IList[]).map(
+                      (list: IList, index: number) => {
+                        return (
+                          <>
+                            <div className="flex flex-col gap-1" key={list._id}>
+                              <a
+                                href={list.site}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="flex flex-col items-center"
+                              >
+                                <img
+                                  src={`/logos/lists/${list.filename}`}
+                                  alt={list.name}
+                                  className="w-16 h-16 object-cover rounded-full transition-transform duration-200 hover:scale-105"
+                                />
+                                <span className="text-center flex flex-row gap-1 justify-center hover:underline mt-1 ml-3">
+                                  {list.trigram}
+                                  <img
+                                    src={LinkIcon}
+                                    className="w-3 h-3"
+                                    alt="Lien"
+                                  />
+                                </span>
+                              </a>
+                            </div>
+                            {index < (campagne.lists as IList[]).length - 1 && (
+                              <span className="text-xl font-semibold">VS</span>
+                            )}
+                          </>
+                        );
+                      }
+                    )}
                   </div>
 
                   <hr className="my-2 border-gray-300" />
