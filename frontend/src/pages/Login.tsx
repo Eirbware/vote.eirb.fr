56b4b11ddea4832cf5aff94c8f268d868f3ff2e8
Raphaël Bourgouin <raphael.bourgouin@enseirb-matmeca.fr>
2026-02-27 11:21:23 +0100

    Changed login screen text

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -6,7 +6,7 @@ import { Footer } from '@/components';
 
 import EirbwareNoTextLogo from '@assets/logos/eirbware_no_text.png';
 import BdeLogo from '@assets/logos/bde.png';
-import LogoINP from '@assets/logos/bdxINP-monochrome-noir.png';
+//import LogoINP from '@assets/logos/bdxINP-monochrome-noir.png';
 
 export const Login = () => {
   const [errorOnLogin, setErrorOnLogin] = useState<boolean>(false);
@@ -94,16 +94,18 @@ export const Login = () => {
         <h1 className="text-3xl font-semibold mb-4 ">
           Connexion à la Plateforme de Vote en ligne
         </h1>
-        <h2 className="text-xl font-medium mb-3 ">Se connecter avec le CAS</h2>
+        <h2 className="text-xl font-medium mb-3 ">Se connecter avec Eirbconnect</h2>
         <p className="text-gray-600">
-          Le CAS est la solution proposée par Bordeaux INP pour se connecter à
-          l'ensemble des sites pédagogiques.
+          Eirbconnect est la solution proposé par Eirbware pour se connecter à l'ensemble des 
+          sites de l'associatif de l'Enseirb. 
+          Il suffit d'avoir un CAS - un compte permettant de se connecter aux sites pédagogiques de Bordeaux INP -
+          pour pouvoir l'utiliser
         </p>
         <button
           onClick={EirbConnectLogin}
           className="my-4 flex items-center gap-2 bg-[#ffdd03] hover:bg-yellow-400 font-medium py-2 px-6 rounded-lg transition-all duration-300"
         >
-          <img src={LogoINP} alt="Logo INP" className="h-6 w-auto" />
+         {/* <img src={LogoINP} alt="Logo INP" className="h-6 w-auto" /> */}
           Se connecter
         </button>
         <blockquote className="italic text-gray-500 text-sm opacity-80 mb-6">
@@ -118,7 +120,7 @@ export const Login = () => {
 
       {errorOnLogin && (
         <div className="fixed top-0 left-1/2 transform -translate-x-1/2 mt-4 px-4 py-2 bg-red-500 text-white font-bold rounded shadow-lg transition-transform animate-slide-down">
-          Erreur de connexion au CAS. Veuillez réessayer.
+          Erreur d'authentification. Veuillez réessayer.
         </div>
       )}
     </div>
